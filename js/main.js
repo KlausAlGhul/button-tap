e0f7fd8aa7d18ddab7b6d64071ff0ef6b70a9a09
KlausAlGhul <1zageres1@gmail.com>
2022-12-06 12:42:13 +0400

    upd2

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+    document.getElementById('main-button').addEventListener('click', (e) => {
+        document.getElementById('block').classList.add('numbers-show')
+    })
+})
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    document.getElementById('main-button').addEventListener('click', (e) => {
+        document.getElementById('hideContent').classList.add('hide')
+    })
+})
